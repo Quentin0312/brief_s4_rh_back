1aaeb892ae844374451d34b48498548ac6c4f759
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-15 22:55:36 +0400

    passenger_wsgi.py added for prod

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ api = hug.API(__name__)
 # allow_origins à restreindre pour le déploiement
 api.http.add_middleware(CORSMiddleware(api, allow_origins=["*"]))
 
-api_base_url = "/api"
+api_base_url = "/api/employee"
 
-hug.get(api_base_url + "/employee", api=api)(EmployeeC.getDatas)
-hug.post(api_base_url + "/employee", api=api)(EmployeeC.addDatas)
-hug.delete(api_base_url + "/employee", api=api)(EmployeeC.removeDatas)
-hug.put(api_base_url + "/employee", api=api)(EmployeeC.modifyDatas)
+hug.get(api_base_url, api=api)(EmployeeC.getDatas)
+hug.post(api_base_url, api=api)(EmployeeC.addDatas)
+hug.delete(api_base_url, api=api)(EmployeeC.removeDatas)
+hug.put(api_base_url, api=api)(EmployeeC.modifyDatas)

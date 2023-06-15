e867dcffa00c42da30ec13c97ba333cb17a999d4
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-16 00:16:18 +0400

    set to dev mode

--- database/database.py
+++ database/database.py
@@ -10,7 +10,7 @@ except:
 """ Create engine and session communicate with database """
 mode = "prod"  # dev / prod
 
-if mode == "dev":
+if mode == "prod":
     connection_url = (
         "postgresql://"
         + getConfig("user")
